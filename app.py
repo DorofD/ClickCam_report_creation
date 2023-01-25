44f277369bb5da447db8dd964b8ffa8adc8e518e
DorofeevE <legion.0@yandex.ru>
2023-01-25 19:22:22 +0300

    веб интерфейс для парсера

--- app.py
+++ app.py
@@ -18,7 +18,19 @@ def report():
     # if file:
     #     return send_file(file, as_attachment=True)
     flash('Ошибка формирования отчёта', category='error')
-    return render_template('index.html', class1='active', class2='', class3='', class4='')
+    return render_template('index.html', class1='active', class2='')
+
+
+@app.route('/clever_cam', methods=['GET', 'POST'])
+def clever_cam():
+    if request.method == 'POST':
+        entered_data = [request.form['pid'], request.form['cam'],
+                        request.form['date'], request.form['start_time'], request.form['end_time']]
+        result = f"{model.get_cc_count(request.form['pid'], request.form['cam'], request.form['date'], request.form['start_time'], request.form['end_time'])}"
+        return render_template('clever_cam.html', class1='', class2='active', entered_data=entered_data,  result=result)
+    entered_data = ['', '', '', '', '']
+    result = ''
+    return render_template('clever_cam.html', class1='', class2='active', entered_data=entered_data,  result=result)
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -2,6 +2,9 @@ import sqlite3 as sq
 import openpyxl
 import os
 import shutil
+from ftplib import FTP
+import re
+import datetime
 
 
 def db_update(file):
@@ -42,8 +45,9 @@ def db_update(file):
         conn.commit()
         conn.close()
         return True
-    except:
+    except Exception as exc:
         conn.close()
+        print(exc)
         return False
 
 
@@ -92,4 +96,63 @@ def get_report(operator):
         print(exc)
         return False
 
-# create_db()
+
+def get_cc_count(pid, cam, date, start_time, end_time):
+    try:
+        def get_count_in_file(start_time, end_time):
+            count = 0
+            with open('exported_log.txt') as file:
+                for line in file:
+                    if 'Вход' in line:
+                        time_in_line = re.findall(r'..:..:..', line)
+                        time_in_line = datetime.datetime.strptime(
+                            time_in_line[0], '%H:%M:%S')
+                        if start_time < time_in_line < end_time:
+                            count += 1
+            return count
+
+        # pid = input('Enter PID: ')
+        # cam = input('Enter cam: ')
+        # date = input('Enter date (yymmdd): ')
+        start_time = datetime.datetime.strptime(
+            start_time, '%H:%M:%S')
+        end_time = datetime.datetime.strptime(
+            end_time, '%H:%M:%S')
+        final_count = 0
+        local_path = r'C:\Users\Dorofeev.E.BOOKCENTRE\Desktop\ssPyQt5_report_creation\exported_log.txt'
+
+        ftp = FTP()
+        ftp.connect('ftp1', 21)
+        print(ftp.login('clevercam', 'clevercam'))
+        ftp.encoding = 'utf-8'
+        ftp.cwd(f'/{pid}/{cam}/')
+
+        files = ftp.nlst()  # получить названия всех файлов в директории
+        # поиск файлов по дате
+        matching_files = []
+        for file in files:
+            temp = re.findall(f'_{date}_', file)
+            if temp:
+                matching_files.append(file)
+
+        print(matching_files)
+
+        for file in matching_files:
+            if os.path.exists(local_path):
+                os.remove(local_path)
+
+            local_file = open('exported_log.txt', 'wb')
+            ftp.retrbinary(f'RETR {file}', local_file.write, 1024)
+
+            local_file.close()
+            final_count += get_count_in_file(start_time, end_time)
+            os.remove(local_path)
+
+        ftp.close()
+
+        # print('Всего посетителей за указанный период:', final_count)
+        return final_count
+    except Exception as exc:
+        print(exc)
+        return False
+    # create_db()
