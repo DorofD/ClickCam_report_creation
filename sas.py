c3e71c7c22867cf27885788cc93acc57ef93c096
DorofeevE <legion.0@yandex.ru>
2023-02-01 19:13:04 +0300

    bugfix

--- sas.py
+++ sas.py
@@ -11,12 +11,21 @@ import os
 # print(source_path + file_name, dest_path + file_name)
 
 # создание файла excel
-boba = 'boba'
-filepath = rf"C:\Users\Dorofeev.E.BOOKCENTRE\Desktop\ssPyQt5_report_creation\{boba}.xlsx"
-wb = openpyxl.Workbook()
-wb.save(filepath)
+# boba = 'boba'
+# filepath = rf"C:\Users\Dorofeev.E.BOOKCENTRE\Desktop\ssPyQt5_report_creation\{boba}.xlsx"
+# wb = openpyxl.Workbook()
+# wb.save(filepath)
 
 # # # удаление файла
 # if os.path.exists(r'C:\Users\Dorofeev.E.BOOKCENTRE\Desktop\ssPyQt5_report_creation\boba.xlsx'):
 #     os.remove(
 #         r'C:\Users\Dorofeev.E.BOOKCENTRE\Desktop\ssPyQt5_report_creation\boba.xlsx')
+
+
+wb = openpyxl.load_workbook(f'addresses.xlsx')
+sheet = wb.active
+print(sheet.max_row)
+
+for i in range(1, sheet.max_row + 1):
+    print(sheet[f'A{i}'].value)
+    # print(i)
